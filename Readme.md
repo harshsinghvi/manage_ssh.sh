6895e2247c04ebd545d3c4917c7ebf3eec893c3b
Harsh Singhvi <root@harshsinghvi.com>
2023-10-02 17:23:52 +0530

    Update run.sh

--- Readme.md
+++ Readme.md
@@ -3,7 +3,6 @@
 > scripts written by ChatGPT
 > Use at your own risk
 
-
 ``` bash
 curl -sSL "https://raw.githubusercontent.com/harshsinghvi/manage_ssh.sh/master/run.sh" | bash -
 ```
--- run.sh
+++ run.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
-# Function to check if sudo is installed
-check_sudo() {
-    if command -v sudo &> /dev/null; then
-        return 0
-    else
-        return 1
-    fi
-}
+# # Function to check if sudo is installed
+# check_sudo() {
+#     if command -v sudo &> /dev/null; then
+#         return 0
+#     else
+#         return 1
+#     fi
+# }
 
-# Function to execute a command with sudo or directly
-execute_with_sudo_or_directly() {
-    local command_to_run="$*"
+# # Function to execute a command with sudo or directly
+# execute_with_sudo_or_directly() {
+#     local command_to_run="$*"
 
-    if check_sudo; then
-        sudo $command_to_run
-    else
-        $command_to_run
-    fi
-}
+#     if check_sudo; then
+#         sudo $command_to_run
+#     else
+#         $command_to_run
+#     fi
+# }
 
 download_and_install_script() {
     local url="$1"
@@ -41,5 +41,6 @@ cd $HOME
 
 download_and_install_script "https://raw.githubusercontent.com/harshsinghvi/manage_ssh.sh/master/manage_ssh.sh" "manage_ssh.sh"
 
+echo Run sudo ./manage_ssh.sh to execute the script
 # Example: Run a command with sudo or directly
-execute_with_sudo_or_directly bash manage_ssh.sh 
+# execute_with_sudo_or_directly bash manage_ssh.sh 
